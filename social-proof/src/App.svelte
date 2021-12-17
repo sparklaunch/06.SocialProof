791e1bee66d6e664c8533d49d34e2b56ff4eeb6c
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-17 09:10:19 +0900

    first commit

---
+++ social-proof/src/App.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;700&display=swap"
+        rel="stylesheet"
+    />
+</svelte:head>
+<div id="app" />
---
+++ social-proof/src/main.ts
@@ -0,0 +1,7 @@
+import App from './App.svelte';
+
+const app: App = new App({
+	target: <HTMLElement>document.body
+});
+
+export default app;
