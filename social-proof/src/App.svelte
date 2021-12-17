d229e25d604c1f21167169d5ca5f1c7dee24fec3
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-17 09:27:23 +0900

    Add the background patterns

--- social-proof/src/App.svelte
+++ social-proof/src/App.svelte
@@ -1,9 +1,58 @@
+<script lang="ts">
+    $: innerWidth = 0;
+    $: isMobile = innerWidth <= 1440;
+</script>
+
+<svelte:window bind:innerWidth />
 <svelte:head>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link
         href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;700&display=swap"
         rel="stylesheet"
     />
 </svelte:head>
-<div id="app" />
+<div id="app">
+    <div id="background-top">
+        <img
+            src="/assets/bg-pattern-top-{isMobile ? 'mobile' : 'desktop'}.svg"
+            alt="Background Top Pattern"
+        />
+    </div>
+    <div id="background-bottom">
+        <img
+            src="/assets/bg-pattern-bottom-{isMobile
+                ? 'mobile'
+                : 'desktop'}.svg"
+            alt="Background Bottom Pattern"
+        />
+    </div>
+</div>
+
+<style>
+    #app {
+        width: 100vw;
+        height: 100vh;
+        position: relative;
+    }
+    #background-top > img {
+        position: absolute;
+        width: 100%;
+        top: -25vh;
+        left: 0;
+    }
+    #background-bottom > img {
+        position: absolute;
+        width: 100%;
+        bottom: -25vh;
+        right: 0;
+    }
+    @media all and (max-width: 1440px) {
+        #background-top > img {
+            top: 0;
+        }
+        #background-bottom > img {
+            bottom: 0;
+        }
+    }
+</style>
